6ed588a6fb73c10ecefbd0002b7a7d7d0abcf433
loretdemolas <sebast.loret@gmail.com>
2023-07-07 12:11:37 -0400

    .gitignore Added gitignore to keep repo clean
    I_need_a_Yob
    added multiple threads to speed up indexing.
    added database support to keep track of indexed job listings
    removed defunc code that wasn't in use

--- I_need_a_Yob.py
+++ I_need_a_Yob.py
@@ -4,11 +4,28 @@ from threading import Thread
 from queue import Queue
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import sqlite3
 
+# Creates databases
+def create_databases():
+    # Creating logged_urls database
+    connection_logged = sqlite3.connect('logged_urls.db')
+    cursor_logged = connection_logged.cursor()
+    cursor_logged.execute('''CREATE TABLE IF NOT EXISTS logged_urls
+                            (url TEXT PRIMARY KEY, search_term TEXT)''')
+    connection_logged.commit()
+    connection_logged.close()
+
+    # Creating filtered_urls database
+    connection_filtered = sqlite3.connect('filtered_urls.db')
+    cursor_filtered = connection_filtered.cursor()
+    cursor_filtered.execute('''CREATE TABLE IF NOT EXISTS filtered_urls
+                              (url TEXT PRIMARY KEY, status TEXT)''')
+    connection_filtered.commit()
+    connection_filtered.close()
+
+
+# Finds all jobs with the query
 def search_jobs(driver, query, job_queue, logged_urls):
     # Perform the Google search
     driver.get(f"https://www.google.com/search?q={query}")
@@ -28,9 +45,14 @@ def search_jobs(driver, query, job_queue, logged_urls):
         for result in search_results:
             link_element = result.find_element(By.CSS_SELECTOR, "a")
             url = link_element.get_attribute("href")
-            if "boards.greenhouse.io" in url and url not in logged_urls:
-                job_queue.put(url)
-                print("New Results added:", url)
+            if "boards.greenhouse.io" in url:
+                connection = sqlite3.connect('logged_urls.db')
+                cursor = connection.cursor()
+                cursor.execute("SELECT url FROM logged_urls WHERE url = ?", (url,))
+                if cursor.fetchone() is None:
+                    job_queue.put(url)
+                    print("New Results added:", url)
+                    connection.close()
 
         # Check if we have reached the bottom of the page
         new_height = driver.execute_script("return document.body.scrollHeight")
@@ -38,44 +60,40 @@ def search_jobs(driver, query, job_queue, logged_urls):
             break
         last_height = new_height
 
+# Records URL to persist results to subsequent searches
+def log_jobs(job_queue, logged_urls, query):
+    connection = sqlite3.connect('logged_urls.db')
+    cursor = connection.cursor()
 
-def log_jobs(driver, job_queue, logged_urls):
-    # Open a text file to save the job details
-    with open("job_list.txt", "a", encoding="utf-8") as file:
-        while not job_queue.empty():
-            job_url = job_queue.get()
-            # Load the job application page
-            driver.get(job_url)
-            #Check to see if apply button exists
+    while True:
+        job_url = job_queue.get()
+        if job_url is None:
+            break
 
-            # Preview the job details
-            job_title = driver.find_element(By.CSS_SELECTOR, "h1.app-title").text
-            company_name = driver.find_element(By.CSS_SELECTOR, "span.company-name").text
-            job_location = driver.find_element(By.CSS_SELECTOR, "div.location").text
+        # Write job details to the SQLite database
+        cursor.execute("INSERT INTO logged_urls (url, search_term) VALUES (?, ?)", (job_url, query))
+        connection.commit()
 
-            # Print job details
-            print(f"Job Title: {job_title}")
-            print(f"Company: {company_name}")
-            print(f"Location: {job_location}")
-            print("----------------------")
+        # Add the URL to the logged URLs set
+        logged_urls.add(job_url)
 
-            # Write job details to the text file
-            file.write(f"Job Title: {job_title}\n")
-            file.write(f"Company: {company_name}\n")
-            file.write(f"Location: {job_location}\n")
-            file.write("----------------------\n")
-
-            # Add the URL to the logged URLs
-            logged_urls.add(job_url)
+    connection.close()
 
 def main():
     try:
+        create_databases()
+
         # Set up the web driver
         driver = webdriver.Chrome()  # Adjust based on your browser and driver choice
         driver.implicitly_wait(10)  # Add an implicit wait to handle page loading
 
-        # User input for Google query
-        query = input("Enter your Google query: ") # site:greenhouse.io "remote"
+        # Input for Google query
+        site = "greenhouse.io"
+        job = "Event Planner"
+        location = "Orlando"
+        time_filter = "last 7 days"
+        query = 'site:' + site + ' "' + job + '" "' + location + '" ' + time_filter
+        print(query)
 
         # Create a queue to store job URLs
         job_queue = Queue()
@@ -88,13 +106,14 @@ def main():
         search_thread.start()
 
         # Create and start the log thread
-        log_thread = Thread(target=log_jobs, args=(driver, job_queue, logged_urls))
+        log_thread = Thread(target=log_jobs, args=(job_queue, logged_urls, query))
         log_thread.start()
 
         # Wait for the search thread to finish
         search_thread.join()
 
         # Wait for the log thread to finish
+        job_queue.put(None)
         log_thread.join()
 
         print("Job details saved to 'job_list.txt'")
@@ -102,13 +121,14 @@ def main():
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         traceback.print_exc()
-    
+
     finally:
         # Close the web driver
         driver.quit()
 
 if __name__ == "__main__":
     main()
+
 
 
     # Iterate over job URLs
